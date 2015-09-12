99e4e45d8155f4b54bd2d5f113cbf08f1173376c
Rusheel Shahani <rush.shahani@gmail.com>
2015-09-12 02:47:11 -0400

    Added node files

---
+++ app/api.js
@@ -0,0 +1,24 @@
+var config = require('../config');
+
+var express = require('express');
+
+var app_id = config.application_id;
+
+var javascript_key = config.javascript_key;
+
+var omdb = require('omdb');
+
+var api = express.Router();
+
+api.get('/update', function(req, res) {
+    console.log('Parsing and updating basic content')
+    omdb.get({title: "Game of thrones"}, false, function(err,movie) {
+        console.log(movie);
+    });
+})
+
+
+module.exports = api;
+
+
+
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	"application_id": "iUvXIcSRjpuB5T2EXnm4gRkG8CQEJuqHQlv0TQHw",
+	"javascript_key": "WNPBaSmGq6jNsi5LjfWhJQCCgYG2idznPV3DCfkh",
+	"port": process.env.PORT || 8080,
+	"secretKey" : "SECRETKEYLALALALLA"
+}
